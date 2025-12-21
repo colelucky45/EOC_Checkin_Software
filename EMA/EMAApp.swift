52aa949c1dedb1983c3bcfecdd9563b81a5fc7bb
colelucky <151252527+colelucky@users.noreply.github.com>
2025-12-21 15:23:19 -0600

    Initial Commit

---
+++ EMA/EMAApp.swift
@@ -0,0 +1,17 @@
+//
+//  EMAApp.swift
+//  EMA
+//
+//  Created by Cole Lucky on 12/21/25.
+//
+
+import SwiftUI
+
+@main
+struct EMAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
